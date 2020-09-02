76e36112cdcc4852018a3d42451e06b379903751
egibbs834 <edwardgibbons34@gmail.com>
2020-09-02 17:40:35 -0400

    added 4 tables for supervillain teams and populated 3 villains each

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -19,3 +19,72 @@ create table heroes(
 
   PRIMARY KEY(id)
 );
+
+
+create table villain_intelligence(
+  id int auto_increment not null,
+  name varchar(50) unique not null,
+  hero_id int unique not null,
+  intel int not null,
+  strength int not null,
+  speed int not null,
+  durability int not null,
+  power int not null,
+  combat int not null,
+  total_power int not null,
+  alignment varchar(50),
+  img_url varchar(1000),
+
+  PRIMARY KEY(id)
+);
+
+create table villain_strength(
+  id int auto_increment not null,
+  name varchar(50) unique not null,
+  hero_id int unique not null,
+  intel int not null,
+  strength int not null,
+  speed int not null,
+  durability int not null,
+  power int not null,
+  combat int not null,
+  total_power int not null,
+  alignment varchar(50),
+  img_url varchar(1000),
+
+  PRIMARY KEY(id)
+);
+
+create table villain_speed(
+  id int auto_increment not null,
+  name varchar(50) unique not null,
+  hero_id int unique not null,
+  intel int not null,
+  strength int not null,
+  speed int not null,
+  durability int not null,
+  power int not null,
+  combat int not null,
+  total_power int not null,
+  alignment varchar(50),
+  img_url varchar(1000),
+
+  PRIMARY KEY(id)
+);
+
+create table villain_total(
+  id int auto_increment not null,
+  name varchar(50) unique not null,
+  hero_id int unique not null,
+  intel int not null,
+  strength int not null,
+  speed int not null,
+  durability int not null,
+  power int not null,
+  combat int not null,
+  total_power int not null,
+  alignment varchar(50),
+  img_url varchar(1000),
+
+  PRIMARY KEY(id)
+);
