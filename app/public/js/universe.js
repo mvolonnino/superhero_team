185d6fae83b9e40d712cde634be23af036365a0a
Stovencrantz <sknapp143@gmail.com>
2020-09-03 18:16:42 -0400

    Completed display, routes, and styling for dynamically generated cards for each hero in our Universe

--- app/public/js/universe.js
+++ app/public/js/universe.js
@@ -5,9 +5,56 @@ $(document).ready(() => {
         .then(response => {
             console.log("All our heroes: ", response);
             console.log("Hero array length: ", response.length);
-            var newRow = $(".firstRow");
+            
+            var newRow = $("#rowWrapper");
+
+
             for(var i = 0; i < response.length; i++){
-               newRow.append($("<div class='card heroCard'>").text(response[i].name));
+
+                var newCol = $("<div class='col mt-4'>");
+                var heroCard = $("<div class='card'>");
+                var heroImg = $("<img src='' alt='' class='card-img-top img-thumbnail' id='hero_img'>");
+                var heroBody = $("<div id='card_body' class='card-body'>");
+                var heroName = $("<h5 id='hero_name'class='card-title'>");
+                var heroInt = $("<p id='heroInt'class='card-text' >");
+                var heroStrength= $("<p id='heroStrngth' class='card-text'>");
+                var heroSpeed= $("<p id='heroSpeed' class='card-text'>");
+                var heroDurability= $("<p id='heroDurability' class='card-text'>");
+                var heroPower= $("<p id='heroPower' class='card-text'>");
+                var heroCombat= $("<p id='heroCombat' class='card-text'>");
+                var fightBtn = $("<button class='btn btn-secondary fightBtn'>").text("Fight!");
+
+                heroCard.append(heroImg);
+                heroImg.attr("src", response[i].img_url).attr("alt", "Image of " + response[i].name);
+                
+                heroName.text(response[i].name);
+                heroBody.append(heroName);
+
+                heroInt.text("Intelligence: "+ response[i].intel);
+                heroBody.append(heroInt);
+
+                heroStrength.text("Strength: "+response[i].strength);
+                heroBody.append(heroStrength);
+
+                heroSpeed.text("Speed: "+response[i].speed);
+                heroBody.append(heroSpeed);
+
+                heroDurability.text("Durability: "+response[i].durability);
+                heroBody.append(heroDurability);
+
+                heroPower.text("Power: "+response[i].power);
+                heroBody.append(heroPower);
+
+                heroCombat.text("Combat: "+response[i].combat);
+                heroBody.append(heroCombat);
+
+                heroBody.append(fightBtn);
+
+              
+            heroCard.append(heroBody);
+               newCol.append(heroCard);
+               newRow.append(newCol);
             }
     })
+
 });
