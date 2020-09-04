4e04ad3dcb7a3502a59157a458d93fb7ad40322e
Matt Volonnino <mvolonnino12@gmail.com>
2020-09-04 11:53:22 -0400

    added jawsdb to connectionMYSQL

--- app/config/connectmySQL.js
+++ app/config/connectmySQL.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql");
 require("dotenv").config();
+var connection;
 
 // connect to database
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: process.env.MYSQL_KEY,
-  database: "hero_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: process.env.MYSQL_KEY,
+    database: "hero_db",
+  });
+}
 
 // clg that we are connected to database
 connection.connect(function (err) {
