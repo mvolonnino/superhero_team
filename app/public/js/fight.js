6f1f4084ef43191e386c04cc891b0e0725a5193c
Matt Volonnino <mvolonnino12@gmail.com>
2020-09-04 03:42:41 -0400

    got the battlegrounds portion of universe page to load up correctly and show the hero and villain character models in there cards with a battleground intbetween

--- app/public/js/fight.js
+++ app/public/js/fight.js
@@ -1,8 +1,53 @@
 $(document).ready(() => {
-  var heroArr = [];
-  var villainArr = [];
+  // $("#rowWrapper").show();
+  $("#battlegroundRow").hide();
+  $("#battleground").hide();
+  $(document).on("click", ".fightBtn", function () {
+    event.preventDefault();
+    // var dataId = $(this).attr("data-id");
+    var img = $(this).parent().siblings("#hero_img").attr("src");
+    var name = $(this).siblings("#hero_name").text();
+    var intel = $(this).siblings("#heroInt").text().split(" ")[1];
+    var strength = $(this).siblings("#heroStrength").text().split(" ")[1];
+    var speed = $(this).siblings("#heroSpeed").text().split(" ")[1];
+    var durability = $(this).siblings("#heroDurability").text().split(" ")[1];
+    var power = $(this).siblings("#heroPower").text().split(" ")[1];
+    var combat = $(this).siblings("#heroCombat").text().split(" ")[1];
+    var alignment = $(this).siblings("#heroAlignment").text().split(" ")[1];
+    var total_power = $(this).siblings("#heroTotalPower").text().split(" ")[2];
 
-  function makeVillainCharacter() {
+    // create the hero character model
+    const hero = new Character(
+      img,
+      name,
+      alignment,
+      parseInt(total_power),
+      parseInt(intel),
+      parseInt(strength),
+      parseInt(speed),
+      parseInt(power),
+      parseInt(durability),
+      parseInt(combat)
+    );
+
+    confirm(`Are you sure you want to use ${name} to fight?`);
+    if (confirm) {
+      console.log(`${name} will be used to fight`);
+      startFight(hero);
+    } else {
+      console.log(`${name} will not be used to fight`);
+    }
+  });
+
+  function startFight(hero) {
+    $("#rowWrapper").hide();
+    $("#universeMessage").hide();
+    $("#battleground").show();
+    $("#battlegroundRow").show();
+    // window.location.href = "/battlegrounds";
+    // grab villain information that was randomized
+    var img = $("#villain_img").attr("src");
+    console.log("img: ", img);
     var name = $("#villain_name").text();
     var alignment = $("#villainAlignment").text().split(" ")[1];
     var total_power = $("#villainTotalPower").text().split(" ")[2];
@@ -12,7 +57,9 @@ $(document).ready(() => {
     var durability = $("#villainDurability").text().split(" ")[1];
     var power = $("#villainPower").text().split(" ")[1];
     var combat = $("#villainCombat").text().split(" ")[1];
+    // create the villain character model
     const villain = new Character(
+      img,
       name,
       alignment,
       parseInt(total_power),
@@ -23,54 +70,33 @@ $(document).ready(() => {
       parseInt(durability),
       parseInt(combat)
     );
+    console.log("Hero Character", hero);
     console.log("Villain Character: ", villain);
-  }
 
-  $(document).on("click", ".fightBtn", function () {
-    event.preventDefault();
-    var dataId = $(this).attr("data-id");
-    console.log("dataId: ", dataId);
-    var name = $(this).siblings("#hero_name").text();
-    var intel = $(this).siblings("#heroInt").text().split(" ")[1];
-    var strength = $(this).siblings("#heroStrength").text().split(" ")[1];
-    var speed = $(this).siblings("#heroSpeed").text().split(" ")[1];
-    var durability = $(this).siblings("#heroDurability").text().split(" ")[1];
-    var power = $(this).siblings("#heroPower").text().split(" ")[1];
-    var combat = $(this).siblings("#heroCombat").text().split(" ")[1];
-    var alignment = $(this).siblings("#heroAlignment").text().split(" ")[1];
-    console.log("alignment: ", alignment);
-    var total_power = $(this).siblings("#heroTotalPower").text().split(" ")[2];
-    console.log("total power: ", total_power);
+    $("#hero_img").attr("src", hero.img);
+    $("#hero_name").text(hero.name);
+    $("#hero_alignment").text(`Alignment: ${hero.alignment}`);
+    $("#hero_total_power").text(`Total Power: ${hero.total_power}`);
+    $("#hero_attack").text(`Attack: ${hero.attack}`);
+    $("#hero_health").text(`Healh: ${hero.health}`);
 
-    const hero = new Character(
-      name,
-      alignment,
-      parseInt(total_power),
-      parseInt(intel),
-      parseInt(strength),
-      parseInt(speed),
-      parseInt(power),
-      parseInt(durability),
-      parseInt(combat)
-    );
-    console.log("Hero Character: ", hero);
-    confirm(`Are you sure you want to use ${name} to fight?`);
-    if (confirm) {
-      console.log(`${name} will be used to fight`);
-      makeVillainCharacter();
-      startFight(hero);
-    } else {
-      console.log(`${name} will not be used to fight`);
-    }
-  });
-
-  function startFight(hero) {
-    // console.log("this is my hero: ", hero);
-    // redirect to start page
+    $("#villainImg").attr("src", villain.img);
+    $("#villainName").text(villain.name);
+    $("#villain_alignment").text(`Alignment: ${villain.alignment}`);
+    $("#villain_total_power").text(`Total Power: ${villain.total_power}`);
+    $("#villain_attack").text(`Attack: ${villain.attack}`);
+    $("#villain_health").text(`Healh: ${villain.health}`);
+    // $("#hero_intel").text(hero.intel);
+    // $("#hero_strength").text(hero.strength);
+    // $("#hero_speed").text(hero.speed);
+    // $("#hero_durability").text(hero.durability);
+    // $("#hero_power").text(hero.power);
+    // $("#hero_combat").text(hero.combat);
   }
 
   // Character Models
   function Character(
+    img,
     name,
     alignment,
     total_power,
@@ -81,6 +107,7 @@ $(document).ready(() => {
     durability,
     combat
   ) {
+    this.img = img;
     this.name = name;
     this.alignment = alignment;
     this.total_power = total_power;
